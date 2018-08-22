41633ea43730d3e2a1c0605c04b63e22a4ca93b6
Harmen Stoppels <harmenstoppels@gmail.com>
2018-08-22 15:35:41 +0300

    Test maxintfloat for Double16, Double32 and Double 64

--- test/specialvalues.jl
+++ test/specialvalues.jl
@@ -1,4 +1,7 @@
-@testset "maxintfloat $T" for T in (Double32, Double64)
-    @test maxintfloat(T) == maxintfloat(T) + one(T)
-    @test maxintfloat(T) != maxintfloat(T) - one(T)
+@testset "maxintfloat $T" for T in (Double16, Double32, Double64)
+    @test isinteger(maxintfloat(T))
+
+    # Test that the next integer is not representable, while the previous is.
+    @test maxintfloat(T) != (maxintfloat(T) + one(T)) - one(T)
+    @test maxintfloat(T) == (maxintfloat(T) - one(T)) + one(T)
 end
